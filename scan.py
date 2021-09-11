aee33e3582a727f65c4f8d3d9b606120bc54370f
Michael Egorov <michwill@gmail.com>
2021-09-11 11:48:36 +0200

    Multiple values of A and gamma

--- scan.py
+++ scan.py
@@ -2,15 +2,16 @@ from simulate_apy import Simulator
 from multiprocessing import Pool
 import numpy as np
 
-A = 2 * 3**3 * 10000
+As = [2, 10, 50]
 gammas = np.logspace(np.log10(5e-7), np.log10(2e-3), 20)
 
 sim = Simulator()
 
 
-def calc(gamma):
-    res = sim.simulate(A=int(A), gamma=int(gamma * 1e18))[1]
-    print(f'A={A}, gamma={gamma:.3e} -> {res:.3f}')
+def calc(params):
+    A, gamma = params
+    res = sim.simulate(A=int(A * 3**3 * 10000), gamma=int(gamma * 1e18))[1]
+    print(f'A={A:.1f}, gamma={gamma:.3e} -> {res:.3f}')
     return res
 
 
@@ -19,7 +20,9 @@ pool = Pool(4)
 if __name__ == '__main__':
     import pylab
 
-    lds = pool.map(calc, gammas)
+    for A in As:
+        lds = pool.map(calc, [(A, gamma) for gamma in gammas])
+        pylab.semilogx(gammas, lds, label=f'A={A:.1f}')
 
-    pylab.semilogx(gammas, lds)
+    pylab.legend()
     pylab.show()
