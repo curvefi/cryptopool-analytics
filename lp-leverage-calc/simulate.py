b4b9eef7e2724008182d4aef0168f12a8260000b
Michael Egorov <michwill@gmail.com>
2024-10-08 23:05:14 +0200

    Update simulator for leveragoor

--- lp-leverage-calc/simulate.py
+++ lp-leverage-calc/simulate.py
@@ -1,13 +1,19 @@
+import json
 from math import sqrt
+from datetime import datetime
+
+
+pool = None
+price_data = None
 
 
 class AMM:
-    def __init__(self, collateral, debt, leverage, fee, oracle):
+    def __init__(self, collateral, leverage, fee, oracle):
         self.collateral = collateral  # y
-        self.debt = debt
         self.leverage = leverage
         self.fee = fee
         self.p_oracle = oracle
+        self.debt = collateral * oracle * (leverage - 1) / leverage
 
     def set_p_oracle(self, p):
         self.p_oracle = p
@@ -82,6 +88,105 @@ class AMM:
         self.collateral = y_after
         self.debt = x0 - x_after
 
+    def get_value(self):
+        return self.collateral * self.p_oracle - self.debt
+
+
+class Simulator:
+    def __init__(self, filename, ext_fee, add_reverse=False,
+                 log=False, verbose=False, func=(lambda x: x)):
+        """
+        filename - OHLC data in the same format as Binance returns
+        ext_fee - Fee which arb trader pays to external platforms
+        add_reverse - Attach the same data with the time reversed
+        func - postprocessing function for the price
+        """
+        self.filename = filename
+        self.ext_fee = ext_fee
+        self.add_reverse = add_reverse
+        self.load_prices()
+        self.log = log
+        self.verbose = verbose
+        self.ema_time = 0
+        self.emas = []
+        self.func = func
+
+    def load_prices(self):
+        global price_data
+
+        if self.filename.endswith('.gz'):
+            import gzip
+            with gzip.open(self.filename, "r") as f:
+                data = json.load(f)
+        else:
+            with open(self.filename, "r") as f:
+                data = json.load(f)
+
+        # timestamp, OHLC, vol
+        unfiltered_data = [[int(d[0])] + [self.func(float(x)) for x in d[1:6]] for d in data]
+        data = []
+        prev_time = 0
+        for d in unfiltered_data:
+            if d[0] >= prev_time:
+                data.append(d)
+                prev_time = d[0]
+        if self.add_reverse:
+            t0 = data[-1][0]
+            data += [[t0 + (t0 - d[0])] + d[1:] for d in data[::-1]]
+        price_data = data
+
+    def update_emas(self, Texp):
+        if self.ema_time != Texp:
+            self.ema_time = Texp
+            self.emas = []
+            ema = price_data[0][1]
+            ema_t = price_data[0][0]
+            for t, _, high, low, _, _ in price_data:
+                ema_mul = 2 ** (- (t - ema_t) / (1000 * Texp))
+                ema = ema * ema_mul + (low + high) / 2 * (1 - ema_mul)
+                ema_t = t
+                self.emas.append(ema)
+
+    def single_run(self, fee, Texp, leverage):
+        self.update_emas(Texp)
+
+        # Data for prices
+        data = price_data
+        emas = self.emas
+        initial_price = emas[0][1]
+        amm = AMM(collateral=(1.0 * leverage), leverage=leverage, fee=fee, oracle=initial_price)
+        initial_value = amm.get_value() / initial_price**leverage
+
+        losses = []
+
+        for (t, o, high, low, c, vol), ema in zip(data, emas):
+            amm.set_p_oracle(ema)
+            # max_price = amm.p_up(amm.max_band)
+            # min_price = amm.p_down(amm.min_band)
+            high = high * (1 - self.ext_fee)
+            low = low * (1 + self.ext_fee)
+            if high > amm.get_p():
+                amm.trade_to_price(high)
+
+            if low < amm.get_p():
+                amm.trade_to_price(low)
+
+            if self.log or self.verbose:
+                d = datetime.fromtimestamp(t//1000).strftime("%Y/%m/%d %H:%M")
+                current_value = amm.get_value() / ema**leverage
+                loss = current_value / initial_value * 100
+                if self.log:
+                    print(f'{d}\t{o:.2f}\t{ema:.2f}\t{amm.get_p():.2f}\t\t{loss:.2f}%')
+                if self.verbose:
+                    losses.append([t//1000, loss / 100])
+
+        if losses:
+            self.losses = losses
+
+        current_value = amm.get_value() / ema**leverage
+        loss = 1 - current_value / initial_value
+        return loss
+
 
 if __name__ == '__main__':
     from math import log10
@@ -89,9 +194,9 @@ if __name__ == '__main__':
 
     steps = 5000
 
-    amm = AMM(collateral=2.0, debt=1.0, leverage=2, fee=0.000, oracle=1.0)
+    amm = AMM(collateral=2.0, leverage=2, fee=0.000, oracle=1.0)
 
     for p in np.logspace(log10(1), log10(2), steps):
         amm.set_p_oracle(p)
         amm.trade_to_price(p)
-        print(p, amm.collateral, amm.debt)
+        print(p, amm.get_value() / p**2)
