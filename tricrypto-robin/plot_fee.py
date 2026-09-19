56b7e0589e775bfae163051dbb5dfddb9f20ea72
Michael Egorov <michwill@gmail.com>
2026-09-19 18:58:34 +0200

    Robinhood tricrypto params experimenting

---
+++ tricrypto-robin/plot_fee.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import numpy as np
+from simulation_int_many import Curve
+
+
+N = 3
+A = int(3 * (10 - 1) * 1e4)
+gamma = int(0.05 * 1e18)
+
+fee_gamma = 1e-2
+mid_fee = 0.04  # %
+out_fee = 0.1  # %
+
+N_POINTS = 5000
+
+
+def f_fees(A, gamma):
+    D = 3 * 10**18
+    c = Curve(A, gamma, D, N)
+    X = 1e18 * np.linspace(0.7, 1.3, N_POINTS)
+    Y = np.array([c.y(int(x), 0, 1) for x in X])
+    p = (X[1:] - X[:-1]) / (Y[:-1] - Y[1:]) - 1
+    f = fee_gamma / (fee_gamma + 1 - X*Y*1e18 / (D / N) ** N)
+    fees = mid_fee * f + out_fee * (1 - f)
+    return p * 100, fees[1:]
+
+
+if __name__ == '__main__':
+    import pylab
+    pylab.plot(*f_fees(A, gamma))
+    pylab.plot([0, 1], [0, 1], '--', c='gray')
+    pylab.plot([-1, 0], [1, 0], '--', c='gray')
+    pylab.show()
